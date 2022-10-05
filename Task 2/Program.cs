d498034ac349783b1a0c616ca1754abd9030f4e0
AnastasiaChubenko <anastasiatolstikhina@gmail.com>
2022-10-05 21:51:45 +0300

    Добавили код к задаче №2

---
+++ Task 2/Program.cs
@@ -0,0 +1,19 @@
+﻿// программа на вход принимает два числа и выдает, какое число большее, а какое - меньшее
+
+Console.WriteLine("введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+Console.WriteLine("число a больше числа b");
+}
+else if (a < b)
+{
+Console.WriteLine("число a меньше числа b");
+}
+else
+{
+Console.WriteLine("число a равно числу b");
+}
