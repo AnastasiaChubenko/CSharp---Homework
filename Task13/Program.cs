9de477445ef1e68e874ec118fb5e75b4b505afea
AnastasiaChubenko <anastasiatolstikhina@gmail.com>
2022-10-10 15:44:18 +0300

    Добавили код к задаче №13

---
+++ Task13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+Console.WriteLine("введите любое целое положительное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string a_text = Convert.ToString(a);
+if (a_text.Length > 2)
+{
+Console.WriteLine("третья цифра числа - " + a_text[2]);
+}
+else 
+{
+Console.WriteLine("третьей цифры нет");
+}
