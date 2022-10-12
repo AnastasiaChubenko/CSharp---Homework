02949f3e9e3e779ade94dc2073a704af3881c1ff
AnastasiaChubenko <anastasiatolstikhina@gmail.com>
2022-10-12 18:03:44 +0300

    Добавили код к задаче №23

---
+++ Task23/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа принимает на вход число N и выдает таблицу кубов чисел от 1 до N
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if(N > 0)
+   for(int i = 1; i <= N; i++)         
+   {             
+       Console.Write(Math.Pow(i, 3) + " ");
+   }
+
+if(N <= 0)
+   {
+       Console.WriteLine("Введите число больше единицы");
+   }
