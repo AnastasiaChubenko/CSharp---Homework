bab9d4ca3dd3245665688a4453240224c90835b3
AnastasiaChubenko <anastasiatolstikhina@gmail.com>
2022-10-18 12:23:03 +0300

    Добавили код к задаче №27

---
+++ Task27/Program.cs
@@ -0,0 +1,23 @@
+﻿// программа, которая принимает на вход число и выдаёт сумму цифр в числе
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string readInput = Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+ 
+int SumAllDigit(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+ 
+int number = Prompt("Введите число :");
+Console.WriteLine($"Сумма всех цифр в числе {number} = {SumAllDigit(number)}");
